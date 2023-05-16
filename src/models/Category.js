e5d9f44e0a2a68f4cb7f7286b6eb2cec195d5df3
LucasRosa53 <lucasrosa223@hotmail.com>
2023-05-16 15:26:22 -0300

    resolvendo models e feito requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ * @returns 
+ */
+
+const CategorySchema = (sequelize, DataTypes) => {
+    const CategoryTable = sequelize.define ("Category", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+        type: DataTypes.STRING,
+        },
+    });
+    return CategoryTable;
+};
+
+module.exports = CategorySchema;
